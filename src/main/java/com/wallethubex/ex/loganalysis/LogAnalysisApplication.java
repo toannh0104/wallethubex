388ca4ffdfe8a0851a02612da9bc1ee44f9306b7
ToanNH <toannh0401@gmail.com>
2018-10-12 16:30:19 +0700

    Initial commit

---
+++ src/main/java/com/wallethubex/ex/loganalysis/LogAnalysisApplication.java
@@ -0,0 +1,12 @@
+package com.wallethubex.ex.loganalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAnalysisApplication.class, args);
+	}
+}
